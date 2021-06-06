3f0d5ad1ed771ec2057793461e0b6dda0e14f2cb
ryanguo <superryanguo@outlook.com>
2021-06-06 23:17:28 +0800

    can't fix the seq2seq program from tf1 to tf2, try other program

--- brain/seq2seq/seq2seq_wrapper.py
+++ brain/seq2seq/seq2seq_wrapper.py
@@ -1,4 +1,8 @@
-import tensorflow as tf
+#import tensorflow as tf
+import tensorflow_addons as tfa
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
+#import tensorflow_addons as tfa
 import numpy as np
 import sys
 
@@ -42,11 +46,11 @@ class Seq2Seq(object):
             # Basic LSTM cell wrapped in Dropout Wrapper
             self.keep_prob = tf.placeholder(tf.float32)
             # define the basic cell
-            basic_cell = tf.contrib.rnn.core_rnn_cell.DropoutWrapper(
-                    tf.contrib.rnn.core_rnn_cell.BasicLSTMCell(emb_dim, state_is_tuple=True),
+            basic_cell = tf.nn.rnn_cell.DropoutWrapper(
+                    tf.nn.rnn_cell.BasicLSTMCell(emb_dim, state_is_tuple=True),
                     output_keep_prob=self.keep_prob)
             # stack cells together : n layered model
-            stacked_lstm = tf.contrib.rnn.core_rnn_cell.MultiRNNCell([basic_cell]*num_layers, state_is_tuple=True)
+            stacked_lstm = tf.nn.rnn_cell.MultiRNNCell([basic_cell]*num_layers, state_is_tuple=True)
 
 
             # for parameter sharing between training model
@@ -54,6 +58,7 @@ class Seq2Seq(object):
             with tf.variable_scope('decoder') as scope:
                 # build the seq2seq model 
                 #  inputs : encoder, decoder inputs, LSTM cell type, vocabulary sizes, embedding dimensions
+                #can't find the embeding_rnn_seq2seq in tensorflow2.5  -- Ryan
                 self.decode_outputs, self.decode_states = tf.contrib.legacy_seq2seq.embedding_rnn_seq2seq(self.enc_ip,self.dec_ip, stacked_lstm,
                                                     xvocab_size, yvocab_size, emb_dim)
                 # share parameters
@@ -70,7 +75,8 @@ class Seq2Seq(object):
             # weighted loss
             #  TODO : add parameter hint
             loss_weights = [ tf.ones_like(label, dtype=tf.float32) for label in self.labels ]
-            self.loss = tf.contrib.legacy_seq2seq.sequence_loss(self.decode_outputs, self.labels, loss_weights, yvocab_size)
+            #self.loss = tf.contrib.legacy_seq2seq.sequence_loss(self.decode_outputs, self.labels, loss_weights, yvocab_size)
+            self.loss = tfa.seq2seq.sequence_loss(self.decode_outputs, self.labels, loss_weights, yvocab_size)
             # train op to minimize the loss
             self.train_op = tf.train.AdamOptimizer(learning_rate=lr).minimize(self.loss)
 
